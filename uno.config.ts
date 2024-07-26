107dc8fb209bd09f8500a4c7a6ab6e6dda5603e4
truongnd2 <truongnd2@fabbi.io>
2024-07-26 16:32:42 +0700

    chore: view transition

--- uno.config.ts
+++ uno.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from 'unocss'
 
 export default defineConfig({
   // ...UnoCSS options
+  rules: [[/^view-transition-([\w-]+)$/, ([, name]) => ({ 'view-transition-name': name })]]
 })
