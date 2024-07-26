367439f8209b491c2e1f51599891d27c81a8fe36
truongnd2 <truongnd2@fabbi.io>
2024-07-26 17:00:58 +0700

    chore: image lazy loading animation

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,8 +4,10 @@
   </div>
   <NuxtLink :to="'/'">
     <NuxtImg
+      placeholder
+      placeholder-class="blur-xl"
+      :loading="'lazy'"
       :style="{ 'view-transition-name': `item-${111}` }"
-      view-transition-name="ronaldo"
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg/800px-Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg"
       class="h-[600px] w-[500px]"
     />
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <NuxtLink :to="'/contact'">
     <NuxtImg
       :style="{ 'view-transition-name': `item-${111}` }"
-      view-transition-name="ronaldo"
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg/800px-Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg"
       class="h-[300px] w-[250px]"
     />
