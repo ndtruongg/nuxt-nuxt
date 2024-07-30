3a336716caef3b7a2196a408d553a8c71c7ef7ed
truongnd2 <truongnd2@fabbi.io>
2024-07-30 10:39:27 +0700

    feat: add i18n

---
+++ components/LanguageSwitcher.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { locale, locales, setLocale } = useI18n()
+
+const availableLocales = computed(() => {
+  return locales.value
+})
+
+function updateLocale(event: Event) {
+  setLocale((event.target as HTMLSelectElement).value)
+  window.location.reload()
+}
+
+onMounted(() => {
+  const langSwitcher = document.querySelector('#langSwitcher') as HTMLSelectElement
+  langSwitcher.value = locale.value
+})
+</script>
+
+<template>
+  <div class="flex items-center gap-1">
+    Language:
+    <select id="langSwitcher" rounded-md text-sm p-1 @change="updateLocale">
+      <option v-for="loc in availableLocales" :key="loc.code" :value="loc.code" p-1>
+        {{ loc.name }}
+      </option>
+    </select>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -8,28 +8,32 @@
   </NuxtLink>
 
   <button @click="fetchData" class="text-blue rounded-lg border p-2 font-bold uppercase">
-    fetch data
+    {{ $t('Hello Vietnam') }}
   </button>
 
-  <a
-    v-for="post of posts"
-    :key="post.id"
-    href="#"
-    class="mb-10 block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-  >
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      {{ post.title }}
-    </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">
-      {{ post.body }}
-    </p>
-  </a>
+  <div class="grid grid-rows-3 gap-4">
+    <div v-for="post of posts" :key="post.id">
+      <a
+        href="#"
+        class="mb-10 block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {{ post.title }}
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+          {{ post.body }}
+        </p>
+      </a>
+    </div>
+  </div>
+
+  <LanguageSwitcher />
 </template>
 
 <script setup lang="ts">
 const posts = ref<{ id: number; userId: number; title: string; body: string }[] | null>()
 
-posts.value = await api(`/posts`)
+posts.value = await api(`/posts?page=1&limit=10`)
 
 const fetchData = async () => {
   const res = await api('/comments')
