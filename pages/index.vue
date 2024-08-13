d099bc7962d72fdf37934399092e98e26f2c3dba
truongnd2 <truongnd2@fabbi.io>
2024-08-13 16:13:40 +0700

    feat: proxy config

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <NuxtWelcome />
+  <div>hello world</div>
+
+  {{ data }}
+
+  <a href="/api/todos" target="_blank">To the endpoint</a>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { data, status } = await useFetch('/api/todos', { server: true })
+
+console.log(status.value)
+</script>
 
 <style scoped></style>
